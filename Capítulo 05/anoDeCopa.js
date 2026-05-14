4576eaa7b964f66874e12bf1510d7f0a11e69e17
Marlon <marlon.da.silva996@gmail.com>
2026-05-14 20:50:10 -0300

    implementando o escrever() dentro do loop

--- Capítulo 05/anoDeCopa.js
+++ Capítulo 05/anoDeCopa.js
@@ -1,8 +1,12 @@
+import { escrever } from "../Capítulo 02/utilitarios.js"
+
 let anoDeCopa = 1930; // primeira edição da copa
 let edicao = 0;
 
 while (edicao <= 25) { // 25 edições marcam o centenário da copa, já que 25 edições x 4 anos = 100 anos 
-    console.log(`Em ${anoDeCopa} vai ter copa!`);
+    escrever(`Em ${anoDeCopa} vai ter copa!`);
     anoDeCopa += 4;
     edicao += 1;
 }
+
+escrever(`Esses são os anos de copa que marcarão o centenário da competição!`)
