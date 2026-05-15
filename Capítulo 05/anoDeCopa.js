5436dda4fd29e9b38e00494d7d3a9123c71430b8
Marlon <marlon.da.silva996@gmail.com>
2026-05-14 21:14:25 -0300

    pedindo pro usuário definir o limite

--- Capítulo 05/anoDeCopa.js
+++ Capítulo 05/anoDeCopa.js
@@ -3,7 +3,9 @@ import { escrever } from "../Capítulo 02/utilitarios.js"
 let anoDeCopa = 1930; // primeira edição da copa
 let edicao = 0;
 
-while (anoDeCopa <= 2100) {
+let anoLimite = parseInt((prompt("Até que anos devemos mostrar as edições da copa?")))
+
+while (anoDeCopa <= anoLimite) {
     escrever(`Em ${anoDeCopa} vai ter copa, e essa será a ${edicao + 1}ª edição.`);
     anoDeCopa += 4;
     edicao += 1;
