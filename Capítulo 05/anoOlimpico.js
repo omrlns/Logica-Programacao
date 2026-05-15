aa83eebe1960c90c16c3d41e1feef63884fcb260
Marlon <marlon.da.silva996@gmail.com>
2026-05-14 21:35:36 -0300

    repetindo o exercício anterior, mas agora com as olimpiadas

---
+++ Capítulo 05/anoOlimpico.js
@@ -0,0 +1,12 @@
+import { escrever } from "../Capítulo 02/utilitarios.js"
+
+let anoOlimpico = 1896;
+
+let anoLimite = parseInt(prompt("Até que anos devemos mostrar as edições da olímpiadas?"));
+let edicao = 0;
+
+while (anoOlimpico <= anoLimite) {
+    escrever(`Em ${anoOlimpico} vai ter olímpiadas, e essa será a ${edicao + 1}ª edição.`);
+    anoOlimpico += 4;
+    edicao += 1;
+}
