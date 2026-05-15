68b93dc6e0b7d43497c650c9344c118113a77d02
Marlon <marlon.da.silva996@gmail.com>
2026-05-14 21:25:12 -0300

    pedindo pro usuário definir o range inicial

--- Capítulo 05/anoDeCopa.js
+++ Capítulo 05/anoDeCopa.js
@@ -1,12 +1,13 @@
 import { escrever } from "../Capítulo 02/utilitarios.js"
 
-let anoDeCopa = 1930; // primeira edição da copa
+// let anoDeCopa = 1930; // primeira edição da copa
+
+let anoInicial = parseInt(prompt("Informe o ANO INICIAL para podermos calcular: "))
+let anoLimite = parseInt(prompt("Até que anos devemos mostrar as edições da copa?"))
 let edicao = 0;
 
-let anoLimite = parseInt((prompt("Até que anos devemos mostrar as edições da copa?")))
-
-while (anoDeCopa <= anoLimite) {
-    escrever(`Em ${anoDeCopa} vai ter copa, e essa será a ${edicao + 1}ª edição.`);
-    anoDeCopa += 4;
+while (anoInicial <= anoLimite) {
+    escrever(`Em ${anoInicial} vai ter copa, e essa será a ${edicao + 1}ª edição.`);
+    anoInicial += 4;
     edicao += 1;
 }
